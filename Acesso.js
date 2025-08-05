5abb368b9a8fa4d4e082bdfb735fd35da3a23e0b
joaomiguelAFK <joaomiguelribeiro60@gmail.com>
2025-08-04 21:01:17 -0300

    Acesso.js

--- Acesso.js
+++ Acesso.js
@@ -7,19 +7,20 @@
     e.preventDefault();
     showMessage("Você está usando o ClassiFilmes sem conta.");
 
-    // window.location.href = "pagina-sem-conta.html";
+    window.location.href = "pagina-sem-conta.html";
   });
 
   document.querySelectorAll('.option a')[1].addEventListener('click', function (e) {
     e.preventDefault();
     showMessage("Redirecionando para login...");
-    // window.location.href = "login.html";
+    window.location.href = "login.html";
   });
 
   document.querySelectorAll('.option a')[2].addEventListener('click', function (e) {
     e.preventDefault();
     showMessage("Baixe o app na loja oficial.");
-    // window.open("https://link-para-loja-do-app", "_blank");
+
+ window.open("https://link-para-loja-do-app", "_blank");
   });
 
   document.querySelectorAll('.option a')[3].addEventListener('click', function (e) {
@@ -35,6 +36,6 @@
   document.querySelector('.button a').addEventListener('click', function (e) {
     e.preventDefault();
     showMessage("ClassiFilmes é um site com recomendações baseadas em críticas e preferências.");
-    // window.location.href = "sobre.html";
+    window.location.href = "sobre.html";
   });
 </script>
